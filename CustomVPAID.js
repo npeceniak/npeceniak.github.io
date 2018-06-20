e97a4d1f70fe639987e9770f4a50abed7c9e3d5e
Nick Peceniak <npeceniak@spotx.tv>
2018-06-20 10:58:29 -0600

    Remove styling

--- CustomVPAID.js
+++ CustomVPAID.js
@@ -3681,7 +3681,7 @@ spotx.test.VPAIDAd.prototype.renderSlot_ = function()
         this.slot_ = document.createElement('div');
         goog.style.setWidth(this.slot_, this.iWidth);
         goog.style.setHeight(this.slot_, this.iHeight);
-        goog.style.setStyle(this.slot_, "background-color", "#e3e3e3");
+        // goog.style.setStyle(this.slot_, "background-color", "#e3e3e3");
         goog.style.setStyle(this.slot_, "top", "0");
         goog.style.setStyle(this.slot_, "left", "0");
         goog.style.setStyle(this.slot_, "position", "absolute");
